76adf8e03a51ff66d4da03ca82dcf45cf048688d
Tom MacWright <tom@macwright.org>
2016-12-15 16:42:24 -0800

    Webcam support
    This is kind of janky and unfinished but... if you don't have any good
    selfies sitting around or want just one more, it makes that possible!

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ Array.prototype.pick = function() {
   return this[Math.floor(Math.random()*this.length)];
 };
 
+var fs = require('fs');
 var express = require('express');
 var app = module.exports.app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var port = process.env.PORT || 3000;
 var oxford = require('project-oxford');
+console.log(process.env.OXFORD);
 var client = new oxford.Client(process.env.OXFORD);
 var angerList = require('./anger.js');
 var contemptList = require('./contempt.js');
@@ -28,52 +30,66 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function(socket) { 
+
   socket.on('selfiePath', function (selfiePath) {
     selfieUrl = selfiePath;
-    console.log('face url: ' + selfiePath);
     client.emotion.analyzeEmotion({
       url: selfiePath,
-    }).then(function (response) {
-      if (response.length == 0) {
-        socket.emit('nohface');
-        return;
-      }
-      var emotionScores = response[0].scores;
-      var highScore = 0;
-      var highEmotion;
-      for (var i in emotionScores) {
-        if (emotionScores[i] > highScore) {
-          highScore = emotionScores[i];
-          highEmotion = i;
-        }
-      }
-      console.log(highScore);
-      console.log(highEmotion);
-      if (highEmotion == 'anger') {
-        socket.emit('catify', highEmotion, selfiePath, angerList.pick());
-      }
-      if (highEmotion == 'contempt') {
-        socket.emit('catify', highEmotion, selfiePath, contemptList.pick());
-      }
-      if (highEmotion == 'disgust') {
-        socket.emit('catify', highEmotion, selfiePath, disgustList.pick());
-      }
-      if (highEmotion == 'fear') {
-        socket.emit('catify', highEmotion, selfiePath, fearList.pick());
-      }
-      if (highEmotion == 'happiness') {
-        socket.emit('catify', highEmotion, selfiePath, happinessList.pick());
-      }
-      if (highEmotion == 'neutral') {
-        console.log('emitting for neutral');
-        socket.emit('catify', highEmotion, selfiePath, neutralList.pick());
-      }
-      if (highEmotion == 'sadness') {
-        socket.emit('catify', highEmotion, selfiePath, sadnessList.pick());
-      }
-      if (highEmotion == 'surprise') {
-        socket.emit('catify', highEmotion, selfiePath, surpriseList.pick());
-      }
-    });
+    }).then(handleResponse, handleError);
   });
+
+  socket.on('selfieData', function (selfieData) {
+    var d = new Buffer(selfieData.split(',')[1], 'base64');
+    client.emotion.analyzeEmotion({
+      data: d,
+    }).then(handleResponse, handleError);
+  });
+
+  function handleError(error) {
+    console.log(error);
+  }
+
+  function handleResponse(response) {
+    console.log('face gotten');
+    if (response.length == 0) {
+      socket.emit('nohface');
+      return;
+    }
+    var emotionScores = response[0].scores;
+    var highScore = 0;
+    var highEmotion;
+    for (var i in emotionScores) {
+      if (emotionScores[i] > highScore) {
+        highScore = emotionScores[i];
+        highEmotion = i;
+      }
+    }
+    console.log(highScore);
+    console.log(highEmotion);
+    if (highEmotion == 'anger') {
+      socket.emit('catify', highEmotion, angerList.pick());
+    }
+    if (highEmotion == 'contempt') {
+      socket.emit('catify', highEmotion, contemptList.pick());
+    }
+    if (highEmotion == 'disgust') {
+      socket.emit('catify', highEmotion, disgustList.pick());
+    }
+    if (highEmotion == 'fear') {
+      socket.emit('catify', highEmotion, fearList.pick());
+    }
+    if (highEmotion == 'happiness') {
+      socket.emit('catify', highEmotion, happinessList.pick());
+    }
+    if (highEmotion == 'neutral') {
+      console.log('emitting for neutral');
+      socket.emit('catify', highEmotion, neutralList.pick());
+    }
+    if (highEmotion == 'sadness') {
+      socket.emit('catify', highEmotion, sadnessList.pick());
+    }
+    if (highEmotion == 'surprise') {
+      socket.emit('catify', highEmotion, surpriseList.pick());
+    }
+  }
 });
