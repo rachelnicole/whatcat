8e0259a7a50660edb83b861895c0aad361b4c158
rachelnicole <loveless@gmail.com>
2017-06-15 15:42:51 -0600

    added better commenting

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-Array.prototype.pick = function() {
-  return this[Math.floor(Math.random()*this.length)];
-};
-
+// This is Node's file system! https://nodejs.org/api/fs.html
 var fs = require('fs');
+// Express is how we handle our server routing https://expressjs.com/
 var express = require('express');
 var app = module.exports.app = express();
 var server = require('http').createServer(app);
+// Socket.IO enables real-time bidirectional event-based communication.
 var io = require('socket.io').listen(server);
+// The process.env variables are passed in for azure to set automatically
 var port = process.env.PORT || 3000;
+// Project Oxford allows us to implement some cognitive service apis in a simpler way.
 var oxford = require('project-oxford');
+// process env variables are set ion Azure
 var client = new oxford.Client(process.env.OXFORD);
 var angerList = require('./anger.js');
 var contemptList = require('./contempt.js');
@@ -18,18 +20,30 @@ var happinessList = require('./happiness.js');
 var neutralList = require('./neutral.js');
 var sadessList = require('./sadness.js');
 var surpriseList = require('./surprise.js');
+// Setting a global selfieUrl so each time it runs, this data is overwritten.
 var selfieUrl;
 
+// This is just our random picker that grabs a random item from each of the lists for the emotions
+Array.prototype.pick = function() {
+  return this[Math.floor(Math.random()*this.length)];
+};
+
+// Have our server listen on the port we define above so we know when connections are made
 server.listen(port);
 
+// Telling our application to use the public folder to serve up static files.
 app.use('/public', express.static('public'));
 
+// When a connection is made to the app, serve up index.html
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+
+// Once a connection is made to the server, open up the socket communication.
 io.on('connection', function(socket) { 
 
+  // When the client emits selfiePath, we are getting the image data passed to the server where we save it locally and send it over to the emotion api to analyze.
   socket.on('selfiePath', function (selfiePath) {
     selfieUrl = selfiePath;
     var data = selfieUrl.replace(/^data:image\/\w+;base64,/, "");
@@ -40,23 +54,19 @@ io.on('connection', function(socket) {
     }).then(handleResponse, handleError);
   });
 
-  socket.on('selfieData', function (selfieData) {
-    var d = new Buffer(selfieData.split(',')[1], 'base64');
-    client.emotion.analyzeEmotion({
-      data: d,
-    }).then(handleResponse, handleError);
-  });
-
+  // log errors to the console
   function handleError(error) {
     console.log(error);
   }
 
   function handleResponse(response) {
     console.log('face gotten');
+    // checking to see if the api actually finds a face, if it doesn't it emits nohface to the client which updates the site with a message to try again
     if (response.length == 0) {
       socket.emit('nohface');
       return;
     }
+    // If a face is found, we iterate through all of the emotion scores comparing each one to the next, until we know which emotion has the highest number.
     var emotionScores = response[0].scores;
     var highScore = 0;
     var highEmotion;
@@ -68,6 +78,8 @@ io.on('connection', function(socket) {
     }
     console.log(highScore);
     console.log(highEmotion);
+
+    // checking to see which the highest emotion is, where we then send a socket message to the client where it picks a random cat from each emotion list and updates the site with the relevant information
     if (highEmotion == 'anger') {
       socket.emit('catify', highEmotion, angerList.pick());
     }
